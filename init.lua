2adba0d214e4df140b73d61b2aa500f1c6a86e61
Ablasse Kingcaid-Ouedraogo <158793596+ablassejr@users.noreply.github.com>
2025-09-18 10:25:12 -0500

    Finalizing Configuration

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
-<<<<<<< HEAD
-=======
--- bootstrap lazy.nvim, LazyVim and your plugins
->>>>>>> 803bc181d7c0d6d5eeba9274d9be49b287294d99
 require("config.lazy")
+require('nvim-ts-autotag').setup({
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  per_filetype = {
+    ["html"] = {
+      enable_close = false
+    }
+  }
+})
+vim.g.lazyvim_check_order = false
